b90b56754f231f07700d55c194611bf071e63a32
rubens <rubensluizlobo@gmail.com>
2024-11-21 23:34:18 -0300

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,24 @@ function App() {
   const hidePass = '../img/hidden.png'
   const showPass = '../img/show.png'
   let visiblePass = false
+  const [typePass,setTypePass] = useState('password')
   const [passIcon,setPassIcon] = useState(hidePass)
   function changePassVisibilityState(){
-    setPassIcon(visiblePass?hidePass:showPass)
     visiblePass = !visiblePass
+    if(visiblePass){
+      setTypePass('text')
+      setPassIcon(showPass)
+    } else {
+      setTypePass('password')
+      setPassIcon(hidePass)
+    }
   }
   return (
     <>
       <div>
         <img src={passIcon} onClick={changePassVisibilityState} />
       </div>
+      <input type={typePass} value={"Esse é um texto teste"}/>
     </>
   )
 }
